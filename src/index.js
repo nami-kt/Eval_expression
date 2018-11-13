a39ec376720070ae83b438ad0d42c90518cc5860
nami-kt <43203057+nami-kt@users.noreply.github.com>
2018-11-13 18:37:10 +0100

    Stage 1
    without * /

--- src/index.js
+++ src/index.js
@@ -1,10 +1,47 @@
 function solveExpression() {
-  var vyraz = document.getElementById("vyraz").value;
-  document.getElementById("vysledek").innerHTML = "Vysledek je: " + vyraz;
+  var expression = document.getElementById("expression").value;
+  let left = 0,
+    right = 0;
+  var isFirst = true;
+  var operator = "+";
+  var result = 0;
+
+  for (let i = 0; i < expression.length; i++) {
+    if (
+      expression.charAt(i) === "+" ||
+      expression.charAt(i) === "-" ||
+      expression.charAt(i) === "*" ||
+      expression.charAt(i) === "/"
+    ) {
+      if (!isFirst) {
+        left = mathUp[operator](left, right);
+        right = 0;
+      } else {
+        isFirst = false;
+      }
+
+      operator = expression.charAt(i);
+    } else {
+      if (isFirst) {
+        left = left * 10 + parseInt(expression.charAt(i));
+      } else {
+        right = right * 10 + parseInt(expression.charAt(i));
+      }
+    }
+  }
+
+  result += mathUp[operator](left, right);
+  document.getElementById("result").innerHTML = "Vysledek je: " + result;
 }
 
-var input = document.getElementById("vyraz");
+const mathUp = {
+  "/": (a, b) => a / b,
+  "*": (a, b) => a * b,
+  "-": (a, b) => a - b,
+  "+": (a, b) => a + b
+};
 
+var input = document.getElementById("expression");
 //spusti f-ci, kdyz uzivatel zmackne enter
 input.addEventListener("keyup", function(event) {
   //zrusi vychozi f-ci klavesy, pokus nejaka existuje
